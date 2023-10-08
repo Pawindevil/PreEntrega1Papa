cfd011078bbc5dbf5700bea4546d4ecef5cb11eb
Gaston Papa <gastonpapaa@hotmail.com>
2023-10-07 21:19:37 -0300

    final preentrega1

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './NavBar.css';
+import CartWidget from '../CartWidget/CartWidget';
+
+const NavBar = () => {
+  return (
+    <header>
+    <h1>Papammerce</h1>
+    <nav>
+      <ul>
+        <li>Lacteos</li>
+        <li>Bebidas</li>
+        <li>Almacen</li>
+      </ul>
+    </nav>
+<CartWidget/>
+</header>
+  )
+}
+
+export default NavBar
